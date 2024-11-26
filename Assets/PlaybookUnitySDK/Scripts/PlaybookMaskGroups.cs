a4c7ed83672663684541408442f8acdf652fee8f
yohanjkang <yohankang0425@gmail.com>
2024-11-26 15:46:12 -0800

    Initial mask pass attempt

--- Assets/PlaybookUnitySDK/Scripts/PlaybookMaskGroups.cs
+++ Assets/PlaybookUnitySDK/Scripts/PlaybookMaskGroups.cs
@@ -1,10 +1,57 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace PlaybookUnitySDK.Scripts
 {
+    [Serializable]
+    public struct PlaybookMaskGroup
+    {
+        [SerializeField]
+        private List<GameObject> maskObjects;
+
+        public int MaskID { get; set; }
+
+        public List<GameObject> MaskObjects => maskObjects;
+    }
+
     public class PlaybookMaskGroups : MonoBehaviour
     {
-        [SerializeField] 
+        [SerializeField]
         private PlaybookMaskGroup[] maskGroups = new PlaybookMaskGroup[7];
+
+        private static readonly int MaskIDProperty = Shader.PropertyToID("_MaskID");
+
+        private void Start()
+        {
+            SetObjectMaskGroups();
+        }
+
+        public void SetObjectMaskGroups()
+        {
+            for (int i = 0; i < maskGroups.Length; i++)
+            {
+                maskGroups[i].MaskID = i;
+                SetMaterialPropertyBlockIDs(maskGroups[i]);
+            }
+        }
+
+        private void SetMaterialPropertyBlockIDs(PlaybookMaskGroup maskGroup)
+        {
+            foreach (GameObject maskObject in maskGroup.MaskObjects)
+            {
+                if (!maskObject.TryGetComponent(out Renderer maskObjectRenderer))
+                {
+                    continue;
+                }
+
+                MaterialPropertyBlock block = new();
+                maskObjectRenderer.GetPropertyBlock(block);
+                Debug.Log($"<color=white>{maskGroup.MaskID}</color>");
+                block.SetInteger(MaskIDProperty, maskGroup.MaskID);
+                maskObjectRenderer.SetPropertyBlock(block);
+                Debug.Log($"<color=red>{block.GetInteger(MaskIDProperty)}</color>");
+            }
+        }
     }
 }
