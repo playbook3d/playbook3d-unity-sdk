51fc8754a170a47384840188a842570ff84e741e
yohanjkang <yohankang0425@gmail.com>
2024-12-13 16:18:23 -0800

    Convert into package

---
+++ Editor/PlaybookSDKEditor.cs
@@ -0,0 +1,139 @@
+using System;
+using PlaybookUnitySDK.Runtime;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace PlaybookUnitySDK.Editor
+{
+    [CustomEditor(typeof(PlaybookSDK))]
+    public class PlaybookSDKEditor : UnityEditor.Editor
+    {
+        private SerializedProperty _playbookAccountAPIKey;
+        private SerializedProperty _framesPerSecond;
+        private SerializedProperty _maxFrames;
+        private SerializedProperty _debugLevel;
+
+        private void OnEnable()
+        {
+            _playbookAccountAPIKey = serializedObject.FindProperty("playbookAccountAPIKey");
+            _framesPerSecond = serializedObject.FindProperty("framesPerSecond");
+            _maxFrames = serializedObject.FindProperty("maxFrames");
+            _debugLevel = serializedObject.FindProperty("sdkDebugLevel");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            PlaybookSDK playbookSDK = (PlaybookSDK)target;
+
+            GUIStyle largeBoldStyle =
+                new(EditorStyles.label) { fontSize = 13, fontStyle = FontStyle.Bold };
+
+            // Render results
+            if (playbookSDK.ResultImageUrls.Count != 0)
+            {
+                GUILayout.Label("Result Image URLs", largeBoldStyle);
+                foreach (string resultImageUrl in playbookSDK.ResultImageUrls)
+                {
+                    GUILayout.Label(resultImageUrl);
+
+                    if (GUILayout.Button("Copy URL"))
+                    {
+                        PlaybookSDK.CopyToClipboard(resultImageUrl);
+                    }
+
+                    EditorUtility.SetDirty(playbookSDK);
+                }
+                GUILayout.Space(20);
+            }
+
+            EditorGUILayout.PropertyField(_playbookAccountAPIKey);
+
+            // Render Properties
+            GUILayout.Space(10);
+            GUILayout.Label("Render Properties", largeBoldStyle);
+            EditorGUILayout.PropertyField(_framesPerSecond);
+            EditorGUILayout.PropertyField(_maxFrames);
+
+            // Workflow Properties
+            GUILayout.Space(10);
+            GUILayout.Label("Workflow Properties", largeBoldStyle);
+
+            string[] teamsList = playbookSDK.GetTeams();
+            string[] workflowsList = playbookSDK.GetWorkflows();
+
+            playbookSDK.CurrTeamIndex = EditorGUILayout.Popup(
+                "Teams",
+                playbookSDK.CurrTeamIndex,
+                teamsList
+            );
+
+            playbookSDK.CurrWorkflowIndex = EditorGUILayout.Popup(
+                "Workflows",
+                playbookSDK.CurrWorkflowIndex,
+                workflowsList
+            );
+
+            // Capture Renders
+            GUILayout.Space(10);
+            GUILayout.Label("Run Workflow", largeBoldStyle);
+
+            EditorGUILayout.PropertyField(_debugLevel);
+            GUILayout.Space(10);
+
+            // Disable buttons if not in Play mode
+            bool isInPlayMode = EditorApplication.isPlaying;
+
+            // Disable buttons if teams not loaded
+            bool teamsLoaded = teamsList[0] != "None";
+
+            if (!isInPlayMode)
+            {
+                GUI.color = Color.red;
+                GUILayout.Label("Enter Play mode to start capturing.", EditorStyles.boldLabel);
+                GUI.color = Color.white;
+            }
+            else if (!teamsLoaded)
+            {
+                GUI.color = Color.red;
+                GUILayout.Label("Credentials have not been loaded in.", EditorStyles.boldLabel);
+                GUI.color = Color.white;
+            }
+
+            bool flags = isInPlayMode && teamsLoaded;
+
+            // Don't allow user to capture image while capturing image sequence
+            GUI.enabled = flags && !playbookSDK.IsCapturingImageSequence;
+            if (GUILayout.Button("Capture Single Frame"))
+            {
+                playbookSDK.InvokeCaptureImage();
+
+                EditorUtility.SetDirty(playbookSDK);
+            }
+
+            GUILayout.Space(10);
+
+            // Enable start capture button if not already capturing
+            GUI.enabled = flags && !playbookSDK.IsCapturingImageSequence;
+            if (GUILayout.Button("Start Capture Image Sequence"))
+            {
+                playbookSDK.StartCaptureImageSequence();
+
+                EditorUtility.SetDirty(playbookSDK);
+            }
+
+            // Enable stop capture button if in the process of capturing
+            GUI.enabled = isInPlayMode && playbookSDK.IsCapturingImageSequence;
+            if (GUILayout.Button("Stop Capture Image Sequence"))
+            {
+                playbookSDK.StopCaptureImageSequence();
+
+                EditorUtility.SetDirty(playbookSDK);
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Runtime/PlaybookCapturePasses.cs
@@ -0,0 +1,273 @@
+using System;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace PlaybookUnitySDK.Runtime
+{
+    /// <summary>
+    /// This class is responsible for capturing the individual render passes and
+    /// saving them to their appropriate files.
+    /// </summary>
+    [RequireComponent(typeof(PlaybookMaskPass))]
+    public class PlaybookCapturePasses : MonoBehaviour
+    {
+        public enum RenderPass
+        {
+            Beauty,
+            Mask,
+            Depth,
+            Outline,
+        }
+
+        public bool IsCapturingImageSequence { get; private set; }
+        public int FrameCount { get; private set; }
+
+        public event Action ImageCaptureComplete;
+        public event Action ImageSequenceCaptureComplete;
+
+        private const string DepthShader = "Shader Graphs/DepthPassShaderGraph";
+        private const string OutlineShader = "Shader Graphs/OutlinePassShaderGraph";
+
+        // Image sequence properties
+        private string _rendersFolderPath;
+
+        private Camera _renderCamera;
+        private PlaybookMaskPass _maskPass;
+        private RenderTexture _beautyPassRenderTexture;
+        private RenderTexture _maskPassRenderTexture;
+        private RenderPassProperty _depthPassProperties;
+        private RenderPassProperty _outlinePassProperties;
+
+        #region Initialization
+
+        private void Awake()
+        {
+            InitializeProperties();
+        }
+
+        /// <summary>
+        /// Initialize the required properties for capturing images and image sequences.
+        /// </summary>
+        private void InitializeProperties()
+        {
+            _renderCamera = GetComponent<Camera>();
+            _maskPass = GetComponent<PlaybookMaskPass>();
+
+            InitializeRenderPasses();
+
+            _rendersFolderPath = PlaybookFileUtilities.GetRendersFolderPath(this);
+        }
+
+        private void InitializeRenderPasses()
+        {
+            Material depthMaterial = new(Shader.Find(DepthShader));
+            Material outlineMaterial = new(Shader.Find(OutlineShader));
+
+            _depthPassProperties = new RenderPassProperty
+            {
+                pass = RenderPass.Depth,
+                texture = new RenderTexture(Screen.width, Screen.height, 32),
+                material = depthMaterial,
+            };
+            _outlinePassProperties = new RenderPassProperty
+            {
+                pass = RenderPass.Outline,
+                texture = new RenderTexture(Screen.width, Screen.height, 32),
+                material = outlineMaterial,
+            };
+
+            _beautyPassRenderTexture = new RenderTexture(Screen.width, Screen.height, 32);
+            _maskPassRenderTexture = new RenderTexture(Screen.width, Screen.height, 32);
+        }
+
+        #endregion
+
+        private void OnDestroy()
+        {
+            PlaybookFileUtilities.DeleteFolderContents(_rendersFolderPath);
+            PlaybookFileUtilities.DeleteFile($"{_rendersFolderPath}.zip");
+        }
+
+        #region Capture Passes
+
+        /// <summary>
+        /// Capture all render passes.
+        /// </summary>
+        public void CaptureRenderPasses()
+        {
+            // Beauty pass
+            CaptureImage(_beautyPassRenderTexture, RenderPass.Beauty);
+
+            // Mask pass
+            CaptureMaskPass();
+
+            // Depth pass
+            CaptureFullscreenPass(_depthPassProperties);
+
+            // Outline pass
+            CaptureFullscreenPass(_outlinePassProperties);
+
+            _renderCamera.targetTexture = null;
+
+            FrameCount++;
+        }
+
+        /// <summary>
+        /// Render an image through the camera attached to this component.
+        /// </summary>
+        private void CaptureImage(RenderTexture renderTexture, RenderPass renderPass)
+        {
+            _renderCamera.targetTexture = renderTexture;
+            _renderCamera.Render();
+
+            SaveImageCaptureAsFile(renderTexture, renderPass);
+        }
+
+        /// <summary>
+        /// Capture the mask pass. This renders gives objects a flat color depending on what
+        /// mask group they're in. If a mask group is not specified, objects will be at default
+        /// placed on the catch-all layer.
+        /// </summary>
+        private void CaptureMaskPass()
+        {
+            _maskPass.SaveProperties();
+            _maskPass.SetProperties();
+
+            CaptureImage(_maskPassRenderTexture, RenderPass.Mask);
+
+            _maskPass.ResetProperties();
+        }
+
+        /// <summary>
+        /// Capture a pass that uses a fullscreen shader as a postprocessing render.
+        /// </summary>
+        private void CaptureFullscreenPass(RenderPassProperty passProperty)
+        {
+            // Clear the previous render
+            GL.Clear(true, true, Color.black);
+
+            CommandBuffer command = new() { name = "CaptureShaderEffect" };
+            command.Blit(null, passProperty.texture, passProperty.material);
+
+            _renderCamera.targetTexture = passProperty.texture;
+
+            // Apply the material during rendering
+            _renderCamera.AddCommandBuffer(CameraEvent.AfterEverything, command);
+            _renderCamera.Render();
+
+            SaveImageCaptureAsFile(passProperty.texture, passProperty.pass);
+
+            _renderCamera.RemoveCommandBuffer(CameraEvent.AfterEverything, command);
+        }
+
+        /// <summary>
+        /// Save the image capture to the renders folder path after appropriately
+        /// naming it.
+        /// </summary>
+        private async void SaveImageCaptureAsFile(RenderTexture renderTexture, RenderPass pass)
+        {
+            RenderTexture.active = renderTexture;
+            Texture2D screenshot =
+                new(renderTexture.width, renderTexture.height, TextureFormat.RGB24, false);
+            screenshot.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
+            screenshot.Apply();
+
+            byte[] bytes = screenshot.EncodeToPNG();
+            string imageName = IsCapturingImageSequence
+                ? $"{pass.ToString()}Pass_{FrameCount}.png"
+                : $"{pass.ToString()}Pass.png";
+            string filePath = Path.Combine(_rendersFolderPath, imageName);
+
+            await File.WriteAllBytesAsync(filePath, bytes);
+
+            RenderTexture.active = null;
+            Destroy(screenshot);
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Separate the images created by pass and zip them into invidual folders.
+        /// </summary>
+        private void SeparateFolderContentsByCapture(string folderPath)
+        {
+            if (!Directory.Exists(folderPath))
+            {
+                PlaybookLogger.LogError($"Folder {folderPath} does not exist.");
+                return;
+            }
+
+            foreach (RenderPass pass in Enum.GetValues(typeof(RenderPass)).Cast<RenderPass>())
+            {
+                string targetFolder = Path.Combine(folderPath, $"{pass.ToString()}Pass");
+
+                // Ensure there is a folder for each render pass type
+                if (!Directory.Exists(targetFolder))
+                {
+                    Directory.CreateDirectory(targetFolder);
+                }
+                else
+                {
+                    PlaybookFileUtilities.DeleteFolderContents(targetFolder);
+                }
+
+                // Move each render pass image to its respective folder
+                string[] matchingImages = Directory.GetFiles(folderPath, $"*{pass}*");
+                foreach (string image in matchingImages)
+                {
+                    string fileName = Path.GetFileName(image);
+                    string destinationPath = Path.Combine(targetFolder, fileName);
+
+                    File.Move(image, destinationPath);
+                }
+
+                PlaybookFileUtilities.ZipFolderContents(targetFolder);
+            }
+        }
+
+        #region Run Workflows
+
+        public void InvokeCaptureImage()
+        {
+            // Ensure previous renders are cleared
+            PlaybookFileUtilities.DeleteFolderContents(_rendersFolderPath);
+
+            CaptureRenderPasses();
+
+            // Notify that capture was completed
+            ImageCaptureComplete?.Invoke();
+        }
+
+        public void StartCaptureImageSequence()
+        {
+            IsCapturingImageSequence = true;
+
+            FrameCount = 0;
+
+            // Ensure previous renders are cleared
+            PlaybookFileUtilities.DeleteFolderContents(_rendersFolderPath);
+        }
+
+        public void StopCaptureImageSequence()
+        {
+            IsCapturingImageSequence = false;
+
+            // Create zips of the image sequences
+            SeparateFolderContentsByCapture(_rendersFolderPath);
+
+            // Notify that capture was completed
+            ImageSequenceCaptureComplete?.Invoke();
+        }
+
+        #endregion
+
+        private struct RenderPassProperty
+        {
+            public RenderPass pass;
+            public RenderTexture texture;
+            public Material material;
+        }
+    }
+}
