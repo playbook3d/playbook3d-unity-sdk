0f576cb19d0a32cc201690cc49c5f43a056c555d
yohanjkang <yohankang0425@gmail.com>
2025-01-29 15:30:10 -0800

    Remove post processing during mask pass

--- Assets/PlaybookUnitySDK/Scripts/PlaybookCapturePasses.cs
+++ Assets/PlaybookUnitySDK/Scripts/PlaybookCapturePasses.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 namespace PlaybookUnitySDK.Scripts
 {
@@ -132,12 +133,16 @@ namespace PlaybookUnitySDK.Scripts
         /// </summary>
         private void CaptureMaskPass()
         {
+            _renderCamera.GetUniversalAdditionalCameraData().renderPostProcessing = false;
+
             _maskPass.SaveProperties();
             _maskPass.SetProperties();
 
             CaptureImage(_maskPassRenderTexture, RenderPass.Mask);
 
             _maskPass.ResetProperties();
+
+            _renderCamera.GetUniversalAdditionalCameraData().renderPostProcessing = true;
         }
 
         /// <summary>
